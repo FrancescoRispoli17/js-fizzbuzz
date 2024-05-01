63124a0c708254832018e54701c680c6497ff730
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-01 20:22:11 +0200

    [UPD] complete

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,24 @@
 'use strict';
 
 const row=document.querySelector('.row');
-let k;
 for(let i = 1; i <= 100; i++){
     const div = document.createElement('div');
     const p = document.createElement('p');
-    if(i % 3 == 0){
+    if((i % 5 == 0) && (i % 3 == 0)){
+        div.classList.add('fizzbuzz');
+        p.append('fizzbuzz');
+    }
+    else if(i % 5 == 0){
+        div.classList.add('buzz');
+        p.append('buzz')
+    }
+    else if(i % 3 == 0){
         div.classList.add('fizz');
-        p.append('fizz')
+        p.append('fizz') 
+    }
+    else{
+        div.classList.add('n');
+        p.append(i) 
     }
     div.classList.add('padding', 'margin', 'text-center');
     p.classList.add('fw-bold', 'fs-4');
