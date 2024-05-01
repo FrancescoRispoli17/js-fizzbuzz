7c46ac8c8be91922833451ef2a4b0802e954cef4
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-01 19:45:52 +0200

    [UPD] fizz quare complete

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,16 @@
 'use strict';
 
 const row=document.querySelector('.row');
+let k;
+for(let i = 1; i <= 100; i++){
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+    if(i % 3 == 0){
+        div.classList.add('fizz');
+        p.append('fizz')
+    }
+    div.classList.add('padding', 'margin', 'text-center');
+    p.classList.add('fw-bold', 'fs-4');
+    div.append(p);
+    row.append(div);
+}
